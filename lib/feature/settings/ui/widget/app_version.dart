30c78e25ffd31c58a8261d936f42fdd378da8f8a
Ivan Trajanovski <ivantrajanovski@gmail.com>
2023-11-11 11:23:30 +0100

    cleanup

--- lib/feature/settings/ui/widget/app_version.dart
+++ lib/feature/settings/ui/widget/app_version.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:package_info_plus/package_info_plus.dart';
-import 'package:scalable_flutter_app_starter/core/ui/widget/labeled_text_button.dart';
-import 'package:scalable_flutter_app_starter/core/util/urls.dart';
 
 class AppVersion extends StatelessWidget {
   const AppVersion({super.key});
@@ -29,11 +27,6 @@ class AppVersion extends StatelessWidget {
             Text(
               'v${data.version} (${data.buildNumber})',
               style: theme.textTheme.bodySmall,
-            ),
-            const LabeledTextButton(
-              label: 'Get the PRO version',
-              action: r'$200 OFF',
-              onTap: Urls.showBuyPro,
             ),
           ],
         );
